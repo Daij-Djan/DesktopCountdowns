3c5585cbeb8167905553298b9abd56ea4d2211cb
Dominik Pich <dominik@pich.info>
2023-05-26 23:27:13 -0700

    add reminders to statusbar too

--- DesktopReminders/Controller/StatusBarItemController.swift
+++ DesktopReminders/Controller/StatusBarItemController.swift
@@ -66,6 +66,38 @@ struct StatusBarItemController {
   }
   
   private func addRemindersToMenu() {
-    // TODO
+    let openRemindersAppAction = #selector(AppDelegate.openRemindersApp(_:))
+
+    // gather dynamic reminder items
+    let reminderItems = reminders.map { reminder in
+      var title = reminder.title
+      if let dueDate = reminder.dueDate {
+        title += " (\(dueDate.daysBetween(Date())))"
+      }
+      title += "..."
+      
+      return NSMenuItem(title: title, action: openRemindersAppAction, keyEquivalent: "")
+    }
+
+    // assemble new menu by
+    // 1. adding old menu items back
+    // 2. adding dynamic items and a sentinal action
+    var allItems = [NSMenuItem]()
+    if let menuItems = menu?.items {
+      for menuItem in menuItems {
+        if menuItem.isSeparatorItem {
+          break
+        }
+        allItems.append(menuItem)
+      }
+    }
+    allItems.append(NSMenuItem.separator())
+    allItems.append(NSMenuItem(title: "REMINDERS", action: nil, keyEquivalent: ""))
+    if !reminderItems.isEmpty {
+      allItems.append(contentsOf: reminderItems)
+      allItems.append(NSMenuItem.separator())
+    }
+    allItems.append(NSMenuItem(title: "Manage...", action: openRemindersAppAction, keyEquivalent: "r"))
+    menu?.items = allItems
   }
 }
